e5e7048d36472b85ef2e7a06d4761a9d0071e94a
Adolfo <adolfo@unizar.es>
2024-08-29 20:10:23 +0200

    Updating subprograms.

--- src/gcf/gcf.h
+++ src/gcf/gcf.h
@@ -1,3 +1,10 @@
+/**
+ * The function gcf calculates the greatest common factor
+ * between two numbers.
+ * 
+ * a y b are the two numbers
+ * The function returns the calculated greatest common factor
+ */
 int gcf(int a, int b) {
 	return ( b==0 ? a : gcf(b,a%b) );
 }
--- src/triangle/repeat.h
+++ src/triangle/repeat.h
@@ -1,5 +1,12 @@
 #include <iostream>
 
+/**
+ * The function repeat outputs the same charater a number of times.
+ * 
+ * n represents the number of times
+ * c is the character that is repeated
+ * The function returns nothing
+ */
 void repeat(int n, char c) {
     for (int i = 0;i<n;++i)
         std::cout<<c;
