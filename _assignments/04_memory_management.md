5b44ef40e20c14e29d499c6bc5803b22f9eeae7e
Darío Suárez Gracia <dario@unizar.es>
2024-09-04 14:27:07 +0200

    Minor update

---
+++ _assignments/04_memory_management.md
@@ -0,0 +1,10 @@
+---
+title: Memory Management and an Introduction to Pointers
+---
+# Prerequisites
+
+# Identifying variables
+
+
+# Fix the code
+
---
+++ src/assigment_04_classes_materials/variables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+namespace { // anonymous namespace
+
+const size_t n = 6;
+unsigned int lynn_conway[n] = {3, 4, 7, 8, 12, 2};
+
+bool is_even(unsigned int a) {
+    unsigned int mask = 0x1;
+    return (a & mask) == 0;
+}
+
+} // anonymous namespace
+
+int main() {
+
+    unsigned int *lynn_ptr=lynn_conway;
+
+    for(size_t i = 0; i < n; ++i) {
+        if(is_even(lynn_ptr[i])) {
+          std::cout << *(lynn_ptr+i) << "is even." << std::endl;
+        }
+    }
+}
