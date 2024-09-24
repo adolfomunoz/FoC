193d269aff8aa7a86b2d25168b0501d51e62f120
Darío Suárez Gracia <dario@unizar.es>
2024-09-24 12:40:22 +0200

    Initial skeleton of the first programs to debug

---
+++ src/assignment_11_classes_materials/factorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+unsigned int factorial(unsigned int n)
+{
+  if(n == 0) {
+    return 1;
+  }
+
+  return factorial(n-1) * n;
+}
+
+int main()
+{
+    std::cout << "Type the n value for computing factorial: ";
+
+    unsigned int n{};
+    std::cin >> n;
+
+    auto result = factorial(n);
+    std::cout << "factorial(" << n << ") = " << result << std::endl;
+
+    return 0;
+}
+
