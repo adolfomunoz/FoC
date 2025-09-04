352893eb06775f888182c3f34c30b41b14d1fc34
Darío Suárez Gracia <dario@unizar.es>
2025-09-04 22:06:53 +0200

    Minor updated on memory management chapter

--- src/scopes.cpp
+++ src/scopes.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-int global_a = 1; // global scope
+int global_a = 1; // global scope, data section
 
 int next(int a) {
-  int local = a + 1; // local scope inside next
-  int* local_ptr = new int; // what is the scope of this variable
+  int local = a + 1; // local scope inside next, stack
+  int* local_ptr = new int; // what is the scope of this variable, heap
   *local_ptr = local;
   return *local_ptr;
 }
 
 int main() {
-  int a = global_a + 1; // local scope inside main
+  int a = global_a + 1; // local scope inside main, stack
   a = next(a);
   std::cout << a << std::endl;
 }
